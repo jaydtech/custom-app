6f3a108c43e9afb9f587858a5e4c528ef9976803
jaydeeptalsania <jaydeep@technerdssolutions.com>
2024-11-22 16:59:18 +0530

    clean index file

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,23 +1,5 @@
-import { useEffect } from "react";
-import { json } from "@remix-run/node";
-import { useFetcher } from "@remix-run/react";
-import {
-  Page,
-  Layout,
-  Text,
-  Card,
-  Button,
-  BlockStack,
-  Box,
-  List,
-  Link,
-  InlineStack,
-} from "@shopify/polaris";
-import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
-import { authenticate } from "../shopify.server";
+import React from "react";
 
 export default function Index() {
- return (
-    <h1>Hello there</h1>
-  );
-};
+  return <h1>Hello there</h1>;
+}
